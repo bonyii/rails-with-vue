857b17af31d8d7347e8f89cd8a94a7de48ea1285
boni <boni@twine.hu>
2021-04-11 17:27:04 +0200

    Add catch all routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root 'home#index'
+
+  get '*all', to: 'home#index', constraints: ->(req) { req.path.exclude? 'rails/active_storage' }
 end
