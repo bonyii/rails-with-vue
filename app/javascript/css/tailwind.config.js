c87f6e169a937a3b9f9efe8c0ee70b61426c351d
boni <boni@twine.hu>
2021-05-17 12:13:43 +0200

    Add Montserrat font

--- app/javascript/css/tailwind.config.js
+++ app/javascript/css/tailwind.config.js
@@ -7,7 +7,27 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: [
+          'Montserrat',
+          'ui-sans-serif',
+          'system-ui',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          '"Segoe UI"',
+          'Roboto',
+          '"Helvetica Neue"',
+          'Arial',
+          '"Noto Sans"',
+          'sans-serif',
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Segoe UI Symbol"',
+          '"Noto Color Emoji"'
+        ]
+      }
+    },
   },
   variants: {
     extend: {},
