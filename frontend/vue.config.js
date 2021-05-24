aa091a5cd293f0aa1f8ea7c4eb5b9d1e798f9b3b
boni <boni@twine.hu>
2021-05-24 17:32:49 +0200

    Add vue config js

---
+++ frontend/vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  devServer: {
+    host: '0.0.0.0',
+    hot: true,
+    disableHostCheck: true
+  },
+  css: {
+    extract: true
+  },
+  outputDir: '../app/assets/javascripts/vue'
+}
